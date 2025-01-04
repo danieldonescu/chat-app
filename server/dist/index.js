13264c6ee12abb2f729abcb426d5a553891d2514
dannydony97 <dannydony97@gmail.com>
2025-01-04 13:57:48 +0200

    first commit

---
+++ server/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const sayMyName = (name) => {
+    console.log(`My name is ${name}`);
+};
+sayMyName("Danii");
+//# sourceMappingURL=index.js.map
---
+++ server/index.ts
@@ -0,0 +1,5 @@
+const sayMyName = (name: string): void => {
+  console.log(`My name is ${name}`);
+};
+
+sayMyName("Danii");
